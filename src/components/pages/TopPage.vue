868b78bba4143c64aa57b3fc0280673510af0af2
asahi-fukuda <asahi.fukuda@myu-on.co.jp>
2023-02-01 16:14:04 +0900

    Spinner

--- src/components/pages/TopPage.vue
+++ src/components/pages/TopPage.vue
@@ -1,16 +1,20 @@
 <template lang="pug">
-.top
-  SinpleButton(text="BUTTON" @click="click")
+.button
+  SimpleButton(text="BUTTON" @click="click")
+.spinner
+  Spinner(:SpinnerWidth="35" :SpinnerHeight="35" :SpinnerSpeed="1.0")
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {defineComponent} from "vue"
 
-import SimpleButton from '../buttons/SimpleButton.vue'
+import SimpleButton from "@/components/buttons/SimpleButton.vue"
+import Spinner from "@/components/indicators/Spinner.vue"
 
 export default defineComponent({
   components: {
     SimpleButton,
+    Spinner
   },
 
   setup() {
