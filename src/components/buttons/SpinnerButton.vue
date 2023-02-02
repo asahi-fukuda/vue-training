642d48b927750f00826f3af9a5ee6447d3352c2e
asahi-fukuda <asahi.fukuda@myu-on.co.jp>
2023-02-02 18:36:21 +0900

    SpinnerButton

---
+++ src/components/buttons/SpinnerButton.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+.button
+  button.btn(:disabled="isProgress")
+    Spinner(:SpinnerWidth="20" :SpinnerHeight="20" :SpinnerSpeed="1.0" v-show="isProgress")
+    svg(xmlns="http://www.w3.org/2000/svg", width="20", height="20", viewBox="0 0 200 200" v-show="isComplete")
+      path(d="M4890.068,1665.319a100,100,0,1,0,100,100A100,100,0,0,0,4890.068,1665.319Zm43.73,51.422,21.414,19.687-74.752,81.3-60.022-61.5,20.819-20.32,38.575,39.52Z", transform="translate(-4790.067 -1665.319)")
+    .text {{ text }}
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+import Spinner from '@/components/indicators/Spinner.vue'
+
+export default defineComponent({
+  components: {
+    Spinner,
+  },
+
+  props: {
+    text: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    isProgress: {
+      type: Boolean as PropType<boolean>,
+      required: true,
+    },
+    isComplete: {
+      type: Boolean as PropType<boolean>,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  background-color: #8a2be2;
+  color: white;
+  border: none;
+  margin: 10px 20px;
+  padding: 12px 18px;
+  border-radius: 8px;
+  cursor: pointer;
+  display: flex;
+  &:hover {
+    background-color: #9932cc;
+  }
+  &:active {
+    opacity: 0.8;
+  }
+  .text {
+    line-height: 20px;
+    margin-left: 5px;
+  }
+}
+</style>
--- src/components/pages/TopPage.vue
+++ src/components/pages/TopPage.vue
@@ -1,24 +1,27 @@
 <template lang="pug">
 .button
   SimpleButton(text="BUTTON" @click="click")
-.spinner
-  Spinner(:SpinnerWidth="35" :SpinnerHeight="35" :SpinnerSpeed="1.0")
 .list
   List
+.button
+  SpinnerButton(text="BUTTON" @click="progress" :isProgress="isProgress" :isComplete="isComplete")
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { ref } from 'vue'
 
 import SimpleButton from '@/components/buttons/SimpleButton.vue'
 import Spinner from '@/components/indicators/Spinner.vue'
 import List from '@/components/lists/List.vue'
+import SpinnerButton from '@/components/buttons/SpinnerButton.vue'
 
 export default defineComponent({
   components: {
     SimpleButton,
     Spinner,
     List,
+    SpinnerButton,
   },
 
   setup() {
@@ -26,8 +29,28 @@ export default defineComponent({
       window.alert('click!')
     }
 
+    const isProgress = ref(false)
+    const isComplete = ref(false)
+
+    // 処理中の挙動
+    const progress = () => {
+      isProgress.value = true
+    }
+
+    // 完了時の挙動
+    const complete = () => {
+      isComplete.value = true
+      setTimeout(() => {
+        isComplete.value = false
+      }, 2000)
+    }
+
     return {
       click,
+      progress,
+      complete,
+      isProgress,
+      isComplete,
     }
   },
 })
